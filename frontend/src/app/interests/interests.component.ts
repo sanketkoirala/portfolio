e8b1aeb34acb94d11f69334516e60f025a9a1ae6
Sanket Koirala <koiralasanket11@gmail.com>
2023-11-05 11:05:56 -0600

    interest component class

---
+++ frontend/src/app/interests/interests.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-interests',
+  templateUrl: './interests.component.html',
+  styleUrls: ['./interests.component.scss']
+})
+export class InterestsComponent {
+  images = ['assets/images/SanketMiami.jpg','assets/images/SevenMagicMountain.jpg','assets/images/RasbPi.jpg','assets/images/SanketVolcano.jpg']
+}
