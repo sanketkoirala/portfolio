14d96c3f33684bd47921ad42c6956215847a411d
Sanket Koirala <koiralasanket11@gmail.com>
2023-11-05 11:09:57 -0600

    app module file

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,11 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { PaidServicesComponent } from './paid-services/paid-services.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { EducationComponent } from './education/education.component';
+import { InterestsComponent } from './interests/interests.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent,
+    CarouselComponent,
+    PaidServicesComponent,
+    AboutComponent,
+    ProjectsComponent,
+    EducationComponent,
+    InterestsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
